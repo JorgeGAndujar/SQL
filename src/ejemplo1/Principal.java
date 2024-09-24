268702cb0c837b53b43105568bb5fd96bb4d2c8f
Jorge <Jorge@DESKTOP-LCQINTR>
2024-09-24 11:57:06 +0200

    PROYECTO PROCEDURE

--- src/ejemplo1/Principal.java
+++ src/ejemplo1/Principal.java
@@ -1,10 +1,30 @@
-
 package ejemplo1;
+
+import java.sql.Connection;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
 
 public class Principal {
 
     public static void main(String[] args) {
-      
+        Connection conexion = ConexionMysql.obtenerConexion();
+        if (conexion != null) {
+            String query = "CALL sumar_dos_numeros_1(?,?,?)";
+            try {
+                CallableStatement cs = conexion.prepareCall(query);
+                cs.setInt("n1_i", 5);
+                cs.setInt("n2_i", 7);
+                cs.registerOutParameter("suma_o", java.sql.Types.INTEGER);
+                cs.execute();
+                int suma = cs.getInt("suma_o");
+                System.out.println("suma: " + suma);
+            } catch (SQLException e) {
+                System.out.println("ERROR DE QUERY" + e);
+            }
+        } else {
+            System.out.println("ERROR DE CONEXION");
+        }
+
     }
-    
+
 }
