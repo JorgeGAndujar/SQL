c9c2c67bac9f933740123711567153d53a1d19a7
Jorge <Jorge@DESKTOP-LCQINTR>
2024-09-25 09:17:39 +0200

    PROYECTO PROCEDURE

--- src/ejemplo1/Mostrar_serie_1.java
+++ src/ejemplo1/Mostrar_serie_1.java
@@ -21,13 +21,13 @@ public class Mostrar_serie_1 {
             cs.setInt(1, 10);  // 'n' es el número de elementos en la serie
 
             // Registrar el parámetro de salida (sumaSerie)
-            cs.registerOutParameter(2, java.sql.Types.VARCHAR);
+            cs.registerOutParameter(2, java.sql.Types.INTEGER);
 
             // Registrar el parámetro de salida para la serie generada (salida)
             cs.registerOutParameter(3, java.sql.Types.VARCHAR);
 
             // Ejecutar el procedimiento
-            cs.execute();
+             boolean hasResultSet = cs.execute();
 
             // Obtener y mostrar la suma de la serie
             int sumaSerie = cs.getInt(2);
@@ -36,6 +36,22 @@ public class Mostrar_serie_1 {
             // Obtener y mostrar la serie generada
             String salida = cs.getString(3);
             System.out.println("Serie generada: " + salida);
+
+            //RECUPERAR LO DEL SELECT
+           
+            if (hasResultSet) {
+                ResultSet rs = cs.getResultSet();
+                while (rs.next()) {
+                    int idAlumno = rs.getInt(1);
+                    String nombre = rs.getString(2);
+                   
+                    
+                    System.out.format("%5d %-20s\n", idAlumno, nombre);
+                }
+                System.out.println("SI HAY RESULTADOS");
+            } else {
+                System.out.println("NO HAY RESULTADOS");
+            }
 
         } catch (SQLException e) {
             System.out.println("ERROR DE QUERY: " + e.getMessage());
