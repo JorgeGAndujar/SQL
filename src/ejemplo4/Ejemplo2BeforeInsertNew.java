36fc83bf48de3c6bab6fcdf5e03ad7de6c66437e
Jorge <Jorge@DESKTOP-LCQINTR>
2024-10-01 09:39:57 +0200

    PROYECTO PROCEDURE

--- src/ejemplo4/Ejemplo2BeforeInsertNew.java
+++ src/ejemplo4/Ejemplo2BeforeInsertNew.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class Ejemplo2AfterInsert {
+public class Ejemplo2BeforeInsertNew {
 
     public static void main(String[] args) {
        Connection conexion = ConexionMysqlBDTRIGGER.obtenerConexion();
